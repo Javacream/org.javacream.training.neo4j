1632fe28ce71227155de0a7f66316d62b0cce735
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2024-02-28 10:54:00 +0100

    add java

---
+++ java/src/main/java/org/javacream/training/neo4j/publishing/web/PublishingWebService.java
@@ -0,0 +1,43 @@
+package org.javacream.training.neo4j.publishing.web;
+
+import org.javacream.training.neo4j.demo.HelloWorld;
+import org.javacream.training.neo4j.publishing.api.Book;
+import org.javacream.training.neo4j.publishing.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PublishingWebService {
+    @Autowired private HelloWorld helloWorld;
+    @Autowired
+    BookRepository bookRepository;
+
+    @PostMapping(path = "api/demo", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String greet(@RequestHeader("message") String message){
+        helloWorld.create(message);
+        return "OK";
+    }
+    @GetMapping(path = "api/demo", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String findAll(){
+        return helloWorld.findAll().toString();
+    }
+    @GetMapping(path = "api/demo/filtered", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String findByMessage(@RequestHeader("filter") String filter){
+        return helloWorld.findByMessage(filter).toString();
+    }
+    @DeleteMapping(path = "api/demo", produces = MediaType.TEXT_PLAIN_VALUE)
+    public void delete(){
+        helloWorld.removeAll();
+    }
+
+    @PostMapping(path = "api/books", consumes = MediaType.APPLICATION_JSON_VALUE) public void createBook(@RequestBody Book book){
+        bookRepository.save(book);
+    }
+    @GetMapping (path = "api/books", produces = MediaType.APPLICATION_JSON_VALUE) public List<Book> findAllBooks(){
+        return bookRepository.findAll();
+    }
+
+}
